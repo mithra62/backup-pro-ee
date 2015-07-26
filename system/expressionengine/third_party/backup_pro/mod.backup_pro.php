e22762d0e1135a161486a26a98879243c591fa3b
Eric Lamb <eric@mithra62.com>
2015-07-26 13:39:34 -0700

    Adds base files and structure

---
+++ system/expressionengine/third_party/backup_pro/mod.backup_pro.php
@@ -0,0 +1,34 @@
+<?php 
+/**
+ * mithra62 - Backup Pro
+ *
+ * @author		Eric Lamb
+ * @copyright	Copyright (c) 2015, mithra62, Eric Lamb.
+ * @link		http://mithra62.com/projects/view/backup-pro/
+ * @version		3.0
+ * @filesource 	./system/expressionengine/third_party/backup_pro/
+ */
+
+require_once PATH_THIRD.'backup_pro/vendor/autoload.php';
+use mithra62\BackupPro\Platforms\Controllers\Eecms;
+use mithra62\BackupPro\Platforms\Controllers\Eecms\Cron;
+
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+include PATH_THIRD.'backup_pro/config'.EXT;
+
+
+/**
+ * Backup Pro - Mod
+ *
+ * Module class
+ *
+ * @package 	mithra62\BackupPro
+ * @author		Eric Lamb
+ * @filesource 	./system/expressionengine/third_party/backup_pro/mod.backup_pro.php
+ */
+class Backup_pro extends Eecms
+{
+    use Cron;
+    
+	public $return_data	= '';
+}
