95369bf37c698e512dfb53ad4cbf0cef93013a7e
Eric Lamb <eric@mithra62.com>
2016-01-31 17:49:29 -0800

    Adds reduced storage and optional folder path

--- system/expressionengine/third_party/backup_pro/views/storage/drivers/_gcs.php
+++ system/expressionengine/third_party/backup_pro/views/storage/drivers/_gcs.php
@@ -1,3 +1,4 @@
+<input type="hidden" value="0" name="gcs_reduced_redundancy" />
 <?php 
 		
 $this->table->add_row(
@@ -13,4 +14,14 @@ $this->table->add_row(
 $this->table->add_row(
     '<label for="gcs_bucket">'.$view_helper->m62Lang('gcs_bucket').'</label><div class="subtext">'.$view_helper->m62Lang('gcs_bucket_instructions').'</div>', 
     form_input('gcs_bucket', $form_data['gcs_bucket'], 'id="gcs_bucket"').$view_helper->m62FormErrors($form_errors['gcs_bucket'])
+);
+
+$this->table->add_row(
+    '<label for="gcs_optional_prefix">'.$view_helper->m62Lang('gcs_optional_prefix').'</label><div class="subtext">'.$view_helper->m62Lang('gcs_optional_prefix_instructions').'</div>', 
+    form_input('gcs_optional_prefix', $form_data['gcs_optional_prefix'], 'id="gcs_optional_prefix"').$view_helper->m62FormErrors($form_errors['gcs_optional_prefix'])
+);
+
+$this->table->add_row(
+    '<label for="gcs_reduced_redundancy">'.$view_helper->m62Lang('gcs_reduced_redundancy').'</label><div class="subtext">'.$view_helper->m62Lang('gcs_reduced_redundancy_instructions').'</div>', 
+    form_checkbox('gcs_reduced_redundancy', '1', $form_data['gcs_reduced_redundancy'], 'id="gcs_reduced_redundancy"').$view_helper->m62FormErrors($form_errors['gcs_reduced_redundancy'])
 );
